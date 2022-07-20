4b93b2e75726a5d3a4da7046704114c503896482
antonio26548 <antonio36548@tukorea.ac.kr>
2022-07-20 10:56:52 -0700

    Initial Commit

---
+++ DesignAsset/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DesignAsset
+//
+//  Created by Park Se in on 2022/07/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DesignAsset/DesignAssetApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignAssetApp.swift
+//  DesignAsset
+//
+//  Created by Park Se in on 2022/07/20.
+//
+
+import SwiftUI
+
+@main
+struct DesignAssetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
