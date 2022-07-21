4f49a4613d160124a069f2d2d4ab6b0dd078be77
antonio26548 <antonio36548@tukorea.ac.kr>
2022-07-21 15:13:24 +0900

    Add short buttonstyle

--- DesignAsset/CustomButtonStyle.swift
+++ DesignAsset/CustomButtonStyle.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct GreenButton: ButtonStyle{
+struct LongGreenButton: ButtonStyle{
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
@@ -18,7 +18,7 @@ struct GreenButton: ButtonStyle{
     }
 }
 
-struct BlueButton:ButtonStyle{
+struct LongBlueButton:ButtonStyle{
     func makeBody(configuration: Configuration) -> some View {
         configuration.label
             .padding()
@@ -28,3 +28,34 @@ struct BlueButton:ButtonStyle{
             .foregroundColor(.white)
     }
 }
+
+struct ShortGreenButton:ButtonStyle{
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .padding()
+            .background(Color(red: 0.366, green: 0.692, blue: 0.457))
+            .cornerRadius(100)
+            .foregroundColor(.white)
+    }
+}
+
+
+struct CustomButtonStyle: View {
+    var body: some View {
+        VStack(alignment: .center) {
+            Button("Hello World"){
+                print("Button Pressed")
+            }.padding().buttonStyle(ShortGreenButton())
+                
+        }
+        .padding()
+    }
+}
+
+struct CustomButtonStyle_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonStyle()
+            
+    }
+}
+
